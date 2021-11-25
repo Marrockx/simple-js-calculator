50fae5ef88c6fd7f8ef2f7b67f7904dc0b13d23a
Mariam Adekola <mariam.adekola10@gmail.com>
2021-11-25 09:21:00 +0100

    display digit on screen

--- js/function.js
+++ js/function.js
@@ -1,6 +1,8 @@
-// const button = document.querySelector('.btn');
+const button = document.querySelector('.calculator-inner-wrap');
+const calcScreen = document.querySelector('.calculator-result-screen')
 
-
-// button.addEventListener('click', function () {
-//     alert('clicked')
-// })
+button.addEventListener('click', function () {
+    // alert(`clicked ${event.target.innerText}`)
+    calcScreen.innerHTML = `${event.target.innerText}`
+    console.log(calcScreen)
+})
